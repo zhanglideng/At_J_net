984c3ffccac47ec73ac2de5eb6e71fe3da7e3aef
random <80976700@qq.com>
2020-05-21 13:20:32 +0800

    修改读入数据的方式。

--- dataloader.py
+++ dataloader.py
@@ -8,6 +8,7 @@ import scipy.io as sio
 import torch
 
 
+# 一次性读入所有数据
 # nyu/test/1318_a=0.55_b=1.21.png
 class AtJDataSet(Dataset):
     def __init__(self, transform1, path=None, flag='train'):
@@ -17,35 +18,45 @@ class AtJDataSet(Dataset):
         self.haze_path, self.gt_path, self.t_path = path
         self.haze_data_list = os.listdir(self.haze_path)
         self.gt_data_list = os.listdir(self.gt_path)
+        self.gt_data_list.sort(key=lambda x: int(x[:-4]))
         self.t_data_list = os.listdir(self.t_path)
-
         self.haze_data_list.sort(key=lambda x: int(x[:-18]))
-
         self.length = len(os.listdir(self.haze_path))
+        self.haze_image_dict = {}
+        self.gth_image_dict = {}
+        self.t_dict = {}
+        self.A_dict = {}
+        # 读入数据
+        A_gth = np.ones((608, 448, 3), dtype=np.float32)
+        for i in range(len(self.haze_data_list)):
+            name = self.haze_data_list[i][:-18]
+            A = float(name[-15:-11])
+            self.haze_image_dict[name] = cv2.imread(self.haze_path + name + '.PNG')
+            t_gth = np.load(self.t_path + name + '.npy')
+            t_gth = np.expand_dims(t_gth, axis=2)
+            t_gth = t_gth.astype(np.float32)
+            self.t_dict[name] = t_gth
+            self.A_dict[name] = A_gth * A
+        for i in range(len(self.gt_data_list)):
+            name = self.gt_data_list[i][:-4]
+            self.gth_image_dict[name] = cv2.imread(self.gt_path + name + '.PNG')
 
     def __len__(self):
         return self.length
 
     def __getitem__(self, idx):
-        haze_image_name = self.haze_data_list[idx]
+        name = self.haze_data_list[idx]
+        haze_image = self.haze_image_dict[name]
+        gt_image = self.gth_image_dict[name]
+        A_gth = self.t_dict[name]
+        t_gth = self.A_dict[name]
 
-        A_gth = np.ones((608, 448, 3), dtype=np.float32)
-        A = float(haze_image_name[-15:-11])
-        A_gth = A_gth * A
-
-        t_gth = np.load(self.t_path + haze_image_name[:-4] + '.npy')
-        t_gth = np.expand_dims(t_gth, axis=2)
-        t_gth = t_gth.astype(np.float32)
-
-        haze_image = cv2.imread(self.haze_path + haze_image_name)
-        gt_image = cv2.imread(self.gt_path + haze_image_name[:-18] + '.PNG')
         if self.transform1:
             haze_image = self.transform1(haze_image)
             gt_image = self.transform1(gt_image)
             A_gth = self.transform1(A_gth)
             t_gth = self.transform1(t_gth)
 
-        print(t_gth[0][0][0])
         haze_image = haze_image.cuda()
         gt_image = gt_image.cuda()
         A_gth = A_gth.cuda()
